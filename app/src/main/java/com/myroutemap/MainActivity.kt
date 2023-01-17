6041d60e4cb541dc7d31aff0c6fc08be27e4322e
Gustavo Maldonado <gamcba7982@gmail.com>
2023-01-17 20:18:12 -0300

    google map added

--- app/src/main/java/com/myroutemap/MainActivity.kt
+++ app/src/main/java/com/myroutemap/MainActivity.kt
@@ -2,10 +2,25 @@ package com.myroutemap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    //se llama automaticamente cuando el mapa se haya cargado
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
     }
 }
+
